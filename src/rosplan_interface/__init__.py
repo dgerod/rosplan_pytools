7e352438ceed1186f365b03985ab44b421f4bef9
Jake Merdich <jake@merdich.com>
2015-07-23 11:28:00 -0700

    Better dispatch support

--- src/rosplan_interface/__init__.py
+++ src/rosplan_interface/__init__.py
@@ -1,7 +1,25 @@
-from .action_interface import start_actions, Action, planner_action
-from .utils import *
-from .kb_interface import *
+# 'noqa' tells the linter to ignore star warnings.
+#   If this file ever becomes complex, remove the
+#   stars and add explicit imports.
+from .action_interface import * # noqa
+from .utils import * # noqa
+from .kb_interface import * # noqa
+from .dispatch_interface import * # noqa
+
 
 def init_rosplan():
+    """
+    Initialize all the things!!
+
+    Loads subscribers and services, assuming you want
+      to work with all of Rosplan. No detriment if
+      that isn't the case, but there are smaller methods
+      for each component to do what you want.
+
+    Since rospy has to be given a node name before
+      anything useful can be done, call this after
+      `rospy.init_node(...)`.
+    """
     start_actions()
     init_kb()
+    init_dispatch()
---
+++ src/rosplan_interface/dispatch_interface.py
@@ -0,0 +1,64 @@
+"""
+  Rosplan Dispatch Interface
+
+  A simple way to set, poll, and interupt plans.
+
+"""
+
+import rospy
+from time import sleep
+from std_msgs.msg import String
+
+
+cmd_pub = None
+
+
+def init_dispatch():
+    global cmd_pub
+    cmd_pub = rospy.Publisher('/kcl_rosplan/planning_commands')
+    rospy.Subscriber('kcl_rosplan/system_state', String, set_dispatch_status)
+
+_status = None
+# quick reference to avoid looking at Rosplan source.
+STATUSES = ['Ready', 'Planning', 'Dispatching', 'Paused']
+
+
+def get_dispatch_status():
+    return _status
+
+
+def set_dispatch_status(msg):
+    global _status
+    _status = msg.data
+
+
+def plan_and_wait():
+    # They also have a service that does this, but I have no idea
+    #   how they react to pauses and cancels.
+    plan()
+    # wait for previous item to finish, if there
+    if get_dispatch_status() != 'Ready':
+        while get_dispatch_status() != 'Ready':
+            sleep(.1)
+    # wait for this to start
+    while get_dispatch_status() == 'Ready':
+        sleep(.1)
+    # wait for item to finish
+    while get_dispatch_status() != 'Ready':
+        sleep(.1)
+
+
+def plan():
+    cmd_pub.publish(String("plan"))
+
+
+def pause():
+    cmd_pub.publish(String("pause"))
+
+
+def cancel():
+    cmd_pub.publish(String("cancel"))
+
+
+def resume():
+    cmd_pub.publish(String("resume"))
