cbd268e68f472dae40953bce89c20c1d342e4be9
Diego Escudero <dgerod@xyz-lab.org.es>
2020-07-17 10:01:49 +0200

    Update planning_system code to work with new version of ROSPlan.

--- src/rosplan_pytools/rosplan/controller/planning_system.py
+++ src/rosplan_pytools/rosplan/controller/planning_system.py
@@ -4,63 +4,97 @@
 """
 
 import rospy
-from time import sleep
-from std_msgs.msg import String
-
-cmd_pub = None
-status = None
-
-# Quick reference to avoid looking at ROSPlan source.
-#   STATUSES = ["Ready", "Planning", "Dispatching", "Paused"]
-#   COMMANDS = ["plan", "pause", "resume", "cancel"]
+from std_srvs.srv import Empty
 
 
-def initialize():
-    global cmd_pub
-    cmd_pub = rospy.Publisher("/kcl_rosplan/planning_commands", String)
-    rospy.Subscriber("kcl_rosplan/system_state", String, set_dispatch_status)
+DEFAULT_PROBLEM_NODE_NAME = "/rosplan_problem_generator"
+DEFAULT_PLANNER_NODE_NAME = "/rosplan_plan_generator"
+DEFAULT_PARSER_NODE_NAME = "/rosplan_plan_parser"
+DEFAULT_DISPATCHER_NODE_NAME = "/rosplan_plan_dispatcher"
+
+_services = {}
 
 
-def is_done():
-    return get_dispatch_status() == "Ready"
+def _wait_until_node_is_ready(service_name):
+    rospy.wait_for_service(service_name)
 
 
-def get_dispatch_status():
-    return status
+def _initialize_problem_services(prefix):
+
+    global _services
+
+    service_name = prefix + "/problem_generation_server"
+    _services["generate_problem"] = \
+        rospy.ServiceProxy(service_name, Empty)
+
+    _wait_until_node_is_ready(service_name)
 
 
-def set_dispatch_status(msg):
-    global status
-    status = msg.data
+def _initialize_planner_services(prefix):
+
+    global _services
+
+    service_name = prefix + "/planning_server"
+    _services["generate_plan"] = \
+        rospy.ServiceProxy(service_name, Empty)
+
+    _wait_until_node_is_ready(service_name)
 
 
-def plan_and_wait():
-    # They also have a service that does this, but I have no idea
-    #   how they react to pauses and cancels.
-    plan()
-    # wait for previous item to finish, if there
-    if get_dispatch_status() != "Ready":
-        while get_dispatch_status() != "Ready":
-            sleep(.1)
-    # wait for this to start
-    while get_dispatch_status() == "Ready":
-        sleep(.1)
-    # wait for item to finish
-    while get_dispatch_status() != "Ready":
-        sleep(.1)
+def _initialize_parser_services(prefix):
+
+    global _services
+
+    service_name = prefix + "/parse_plan"
+    _services["parse_plan"] = \
+        rospy.ServiceProxy(service_name, Empty)
+
+    _wait_until_node_is_ready(service_name)
+
+
+def _initialize_dispatcher_services(prefix):
+
+    global _services
+
+    service_name = prefix + "/dispatch_plan"
+    _services["dispatch_plan"] = \
+        rospy.ServiceProxy(service_name, Empty)
+
+    service_name = prefix + "/cancel_plan"
+    _services["cancel_dispatch"] = \
+        rospy.ServiceProxy(service_name, Empty)
+
+    _wait_until_node_is_ready(prefix + "/cancel_dispatch")
+
+
+def initialize(problem_node_name=None, planner_node_name=None, parser_node_name=None,
+               dispatcher_node_name=None):
+    _initialize_problem_services(problem_node_name or DEFAULT_PROBLEM_NODE_NAME)
+    _initialize_planner_services(planner_node_name or DEFAULT_PLANNER_NODE_NAME)
+    _initialize_parser_services(parser_node_name or DEFAULT_PARSER_NODE_NAME)
+    _initialize_dispatcher_services(dispatcher_node_name or DEFAULT_DISPATCHER_NODE_NAME)
+
+
+def generate_plan():
+    _services["generate_problem"]()
+    _services["generate_plan"]()
+
+
+def execute_plan():
+    _services["parse_plan"]()
+    _services["dispatch_plan"]()
 
 
 def plan():
-    cmd_pub.publish(String("plan"))
+    _services["generate_problem"]()
+    _services["generate_plan"]()
+    _services["parse_plan"]()
+    _services["dispatch_plan"]()
 
 
-def pause():
-    cmd_pub.publish(String("pause"))
+def plan_and_wait():
+    pass
 
 
 def cancel():
-    cmd_pub.publish(String("cancel"))
-
-
-def resume():
-    cmd_pub.publish(String("resume"))
+    _services["cancel_dispatch"]()
